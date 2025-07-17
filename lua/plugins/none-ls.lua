5224384dbbdbd2f6bfbd1fd56239865cdc6c7839
Илья Гришин <withsoull@yadnex.ru>
2025-07-17 16:50:56 +0300

    [feat/nvim/go] add golang auto-formatter (caused by saving)

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,27 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    -- Создаём группу автокоманд один раз
+    local augroup = vim.api.nvim_create_augroup("GoFormat", { clear = true })
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.goimports,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+  end,
+}
